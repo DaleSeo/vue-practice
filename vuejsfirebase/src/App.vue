f0521c7f6fa63d0345a310b1ca0f3bcc54f10a40
Dale Seo <dale.seo@gmail.com>
2017-03-29 19:43:49 +0900

    vuejsfirebase2

--- vuejsfirebase/src/App.vue
+++ vuejsfirebase/src/App.vue
@@ -5,6 +5,28 @@
     </div>
     <div class="panel panel-default">
       <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" @submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
         <h3>Books Lists</h3>
       </div>
       <div class="panel-body">
@@ -17,6 +39,9 @@
               <th>
                 Author
               </th>
+              <th>
+                Delete
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -27,6 +52,9 @@
               <td>
                 {{book.author}}
               </td>
+              <td>
+                <span class="glyphicon glyphicon-trash" @click="removeBook(book)"></span>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -36,9 +64,8 @@
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 import Firebase from 'firebase'
+import toastr from 'toastr'
 
 let config = {
   apiKey: "AIzaSyC6OgS9N0Ikn8Iap-RZ1L6K9RTxpzLNt6I",
@@ -57,6 +84,32 @@ export default {
   name: 'app',
   firebase: {
     books: booksRef
+  },
+  data () {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function (e) {
+      booksRef.push(this.newBook)
+      this.resetBook()
+    },
+    resetBook: function () {
+      this.newBook = {
+        title: '',
+        author: '',
+        url: ''
+      }
+    },
+    removeBook: function (book) {
+      booksRef.child(book['.key']).remove()
+      toastr.success('Book removed')
+    }
   }
 }
 </script>
