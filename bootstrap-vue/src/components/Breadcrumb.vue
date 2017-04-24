b6afba416acbe3ae0b7e6aab39a074450fbf50c1
Dale Seo <dale.seo@gmail.com>
2017-04-24 23:10:41 +0900

    bootstrap-vue

---
+++ bootstrap-vue/src/components/Breadcrumb.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <b-breadcrumb :items="items" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [{
+        text: 'Admin',
+        link: '#',
+      }, {
+        text: 'Manage',
+        link: 'http://google.com',
+      }, {
+        text: 'Library',
+        active: true
+      }]
+    }
+  }
+}
+</script>
